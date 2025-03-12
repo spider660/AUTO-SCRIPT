b2748dfd40b571d646b7c6db93f18ab90928b496
spider660 <spiderspiders049@gmail.com>
2025-03-12 19:14:44 +0300

    Update m-ssws.sh

--- menu/m-ssws.sh
+++ menu/m-ssws.sh
@@ -3,7 +3,7 @@ MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m      • NT Shadowsocks Account •          \E[0m"
+echo -e "\E[0;100;33m      • SPIDER WEB SCRIPT  •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Create Account Shadowsocks "
