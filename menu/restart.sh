899ffe6f57749acfd7e7428d1bcd72b0840b2723
spider660 <spiderspiders049@gmail.com>
2025-03-12 19:15:31 +0300

    Update restart.sh

--- menu/restart.sh
+++ menu/restart.sh
@@ -3,7 +3,7 @@ MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m         • NT RESTART MENU •          \E[0m"
+echo -e "\E[0;100;33m         • SPIDER WEB SCRIPT •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Restart All Services"
